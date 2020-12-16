b0e9aabf3ddeee37fa52f1f1c65e29e3cafb2d32
Shaina C <scailles23@gmail.com>
2020-12-16 04:05:02 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,7 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- if phone.match(?:\+?|\b)[0-9]{10}\b)
+ if phone.match(/?:\+?|\b)[0-9]{10}\b/)
    return true
  else
    false
